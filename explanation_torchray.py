7bb767a322cfc22e59c23d386e56656ba8b232a5
ugebn <ugebn@student.kit.edu>
2021-01-29 14:29:35 +0100

    transfer learning or load model

--- explanation_torchray.py
+++ explanation_torchray.py
@@ -6,17 +6,26 @@ import copy
 import matplotlib.pyplot as plt
 import time
 import os
+import utils
 
 from torchray.attribution.grad_cam import grad_cam
 from torchray.attribution.gradient import gradient
 
+# todo anwendung klären: verschiedene datensätze + modelle, und dann beispielbild aus dataset
+# oder eine architektur und ein beispielbild
+
 data = {
     'Cifar': 'cifar',
-    'Imagenet': 'imagenet', # no longer publicly available
-    'VocDetection': 'voc_2007', #todo update download function
-    'Coco': 'coco' # todo test
-}
+    'Imagenet': 'imagenet',
+    'STL10': 'stl10'
 
+    # stl10
+}
+'''
+    'VocDetection': 'voc_2007', # todo RuntimeError: each element in list of batch should be of equal size (collate_fn)
+    'Coco': 'coco' # todo test
+    Both datasets are part of the pointing game 
+    '''
 model_archs = {
     'VGG': 'vgg16',
     'Resnet': 'resnet50'
@@ -33,13 +42,10 @@ expl_methods = {
     #'rise',
     #'extremal_perturbation'
 }
-dataset_name = data['Cifar']
+dataset_name = data['STL10']
 modelarch_name = model_archs['Resnet']
-expl_method = expl_methods['Gradient']
-
-classnames = ['plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck'] #todo integrate class names somewhere
-
+expl_method = expl_methods['Gradcam']
+transfer_learning = False
 # DATASET
 # Pre-process the image and convert into a tensor
 shape= 224 # defined by model
@@ -51,59 +57,56 @@ transform = torchvision.transforms.Compose([
                                      std=[0.229, 0.224, 0.225]),
 ])
 
-dataset = datasets.get_dataset(name=dataset_name,
+dataset, classnames = datasets.get_dataset(name=dataset_name,
                                subset='train', # train oder val
                                download=True,
-                               transform= transform
+                               transform= transform # transformation dependent on dataset, right?
                                )
-dataset_split = torch.utils.data.Subset(dataset,indices=range(2000))
-dataloader = torch.utils.data.DataLoader(dataset_split, batch_size=4, shuffle=True, num_workers=4)
-# todo work with different batch_sizes, training larger than saliency generation. how ?
+#dataset_split = torch.utils.data.Subset(dataset,indices=range(2000))
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
 
 # IMAGE + CATEGORY_ID
-image, label = dataset.__getitem__(np.random.randint(0,len(dataset.data)))
-
+# image, label = dataset.__getitem__(np.random.randint(0,len(dataset.data)))
 
 # MODEL
 model = models.get_model(arch=modelarch_name,
                          dataset = dataset_name # model for specified dataset
                   )
 
-# PREDICTION
-# model(image) # mismatch of classes. imagenet 1000, cifar 10. therefore transfer learning
-
-# transfer learning
+# TRANSFER LEARNING
 # try convnet as feature extractor, cause vgg much smaller than image net
 model, criterion, optimizer_conv, exp_lr_scheduler = models.transfer_learning_prep(model)
 
-# train_model
-# model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler, dataloader, len(dataset_split), epochs=15)
-
-# save model
 model_path = './models/'
-# if not os.path.exists(model_path): os.mkdir(model_path)
-# torch.save(model.state_dict(), model_path + 'cifar_on_resnet.pth')
+if not os.path.exists(model_path): os.mkdir(model_path)
+model_path = model_path + dataset_name + 'on' + modelarch_name + '.pth'
 
-# load trained model
-model.load_state_dict(torch.load(model_path + 'cifar_on_resnet.pth'))
+if transfer_learning: # todo change to check if model exists
+    # train_model
+    model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler, dataloader, len(dataset), epochs=15)
+    # save model
+    torch.save(model.state_dict(), model_path)
+else:
+    assert os.path.exists(model_path), 'First need to do transfer learning and train a model, before loading it'
+    # load trained model
+    model.load_state_dict(torch.load(model_path))
+    models.visualize_model(model, dataloader, classnames, num_images=6)
 
-models.visualize_model(model, dataloader, classnames, num_images=6)
-
+# EXPLANATION METHOD
 images, labels = iter(dataloader).next()
 if expl_method == 'grad_cam':
     grad_cam_layer = 'layer4'
     saliency = grad_cam(
-        model = model,
-        input = images,
-        target = labels,
-        saliency_layer = grad_cam_layer
-        # resize= ?
+        model=model,
+        input=images,
+        target=labels,
+        saliency_layer=grad_cam_layer
     )
 elif expl_method == 'gradient':
     saliency = gradient(
-        model = model,
-        input = images,
-        target = labels
+        model=model,
+        input=images,
+        target=labels
     )
 else:
     assert False
