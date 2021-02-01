66876d0f7509552245af81a77b927519ed08978a
ugebn <ugebn@student.kit.edu>
2021-02-01 14:22:41 +0100

    working version with several datasets, models and several expln methods

--- explanation_torchray.py
+++ explanation_torchray.py
@@ -1,12 +1,7 @@
 import torchvision
 import torch
 from torchray.benchmark import datasets, models, plot_example
-import numpy as np
-import copy
-import matplotlib.pyplot as plt
-import time
 import os
-import utils
 
 from torchray.attribution.grad_cam import grad_cam
 from torchray.attribution.gradient import gradient
@@ -18,37 +13,29 @@ data = {
     'Cifar': 'cifar',
     'Imagenet': 'imagenet',
     'STL10': 'stl10'
-
-    # stl10
 }
-'''
-    'VocDetection': 'voc_2007', # todo RuntimeError: each element in list of batch should be of equal size (collate_fn)
-    'Coco': 'coco' # todo test
-    Both datasets are part of the pointing game 
-    '''
 model_archs = {
     'VGG': 'vgg16',
     'Resnet': 'resnet50'
 }
-
 expl_methods = {
     #'center',
     #'contrastive_excitation_backprop',
     #'deconvnet',
     #'excitation_backprop',
     'Gradcam': 'grad_cam',
-    'Gradient': 'gradient'
+    'Gradient': 'gradient',
     #'guided_backprop',
     #'rise',
     #'extremal_perturbation'
 }
-dataset_name = data['STL10']
-modelarch_name = model_archs['Resnet']
+dataset_name = data['Cifar']
+modelarch_name = model_archs['VGG']
 expl_method = expl_methods['Gradcam']
-transfer_learning = False
+
 # DATASET
 # Pre-process the image and convert into a tensor
-shape= 224 # defined by model
+shape = 224  # defined by model
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize(shape),
     torchvision.transforms.CenterCrop(shape),
@@ -58,36 +45,35 @@ transform = torchvision.transforms.Compose([
 ])
 
 dataset, classnames = datasets.get_dataset(name=dataset_name,
-                               subset='train', # train oder val
+                               subset='train',  # train oder val
                                download=True,
-                               transform= transform # transformation dependent on dataset, right?
+                               transform= transform  # transformation dependent on dataset, right?
                                )
-#dataset_split = torch.utils.data.Subset(dataset,indices=range(2000))
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
+dataset_split = torch.utils.data.Subset(dataset,indices=range(10000))
+dataloader = torch.utils.data.DataLoader(dataset_split, batch_size=4, shuffle=True, num_workers=4)
 
 # IMAGE + CATEGORY_ID
 # image, label = dataset.__getitem__(np.random.randint(0,len(dataset.data)))
 
 # MODEL
 model = models.get_model(arch=modelarch_name,
-                         dataset = dataset_name # model for specified dataset
+                         dataset=dataset_name  # model for specified dataset
                   )
 
 # TRANSFER LEARNING
 # try convnet as feature extractor, cause vgg much smaller than image net
-model, criterion, optimizer_conv, exp_lr_scheduler = models.transfer_learning_prep(model)
+model, criterion, optimizer_conv, exp_lr_scheduler = models.transfer_learning_prep(model, modelarch_name, len(classnames))
 
 model_path = './models/'
 if not os.path.exists(model_path): os.mkdir(model_path)
-model_path = model_path + dataset_name + 'on' + modelarch_name + '.pth'
+model_path = model_path + dataset_name + '_on_' + modelarch_name + '.pth'
 
-if transfer_learning: # todo change to check if model exists
+if not os.path.exists(model_path):
     # train_model
-    model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler, dataloader, len(dataset), epochs=15)
+    model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler, dataloader, len(dataset_split), epochs=15)
     # save model
     torch.save(model.state_dict(), model_path)
 else:
-    assert os.path.exists(model_path), 'First need to do transfer learning and train a model, before loading it'
     # load trained model
     model.load_state_dict(torch.load(model_path))
     models.visualize_model(model, dataloader, classnames, num_images=6)
@@ -95,7 +81,7 @@ else:
 # EXPLANATION METHOD
 images, labels = iter(dataloader).next()
 if expl_method == 'grad_cam':
-    grad_cam_layer = 'layer4'
+    grad_cam_layer = ''
     saliency = grad_cam(
         model=model,
         input=images,
@@ -109,7 +95,7 @@ elif expl_method == 'gradient':
         target=labels
     )
 else:
-    assert False
+    assert False, 'Explanation Method is not yet defined'
 
 plot_example(images, saliency, expl_method, labels, classnames)
 print('show plot')
