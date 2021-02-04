8b44fc40e4aa3c3e5a61990c7ea7c5e5298692d0
ugebn <ugebn@student.kit.edu>
2021-02-04 15:26:38 +0100

    add meaningful perturbation method and beautify

--- explanation_torchray.py
+++ explanation_torchray.py
@@ -5,9 +5,8 @@ import os
 
 from torchray.attribution.grad_cam import grad_cam
 from torchray.attribution.gradient import gradient
+from torchray.attribution.meaningful_perturbation import train_mask
 
-# todo anwendung klären: verschiedene datensätze + modelle, und dann beispielbild aus dataset
-# oder eine architektur und ein beispielbild
 
 data = {
     'Cifar': 'cifar',
@@ -28,13 +27,13 @@ expl_methods = {
     #'guided_backprop',
     #'rise',
     #'extremal_perturbation'
+    'Meaningful_perturbation' : 'meaningful_perturbation'
 }
 dataset_name = data['Cifar']
 modelarch_name = model_archs['VGG']
-expl_method = expl_methods['Gradcam']
+expl_method = expl_methods['Meaningful_perturbation']
 
 # DATASET
-# Pre-process the image and convert into a tensor
 shape = 224  # defined by model
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize(shape),
@@ -49,37 +48,33 @@ dataset, classnames = datasets.get_dataset(name=dataset_name,
                                download=True,
                                transform= transform  # transformation dependent on dataset, right?
                                )
-dataset_split = torch.utils.data.Subset(dataset,indices=range(10000))
+dataset_split = torch.utils.data.Subset(dataset,indices=range(40))
 dataloader = torch.utils.data.DataLoader(dataset_split, batch_size=4, shuffle=True, num_workers=4)
-
-# IMAGE + CATEGORY_ID
-# image, label = dataset.__getitem__(np.random.randint(0,len(dataset.data)))
 
 # MODEL
 model = models.get_model(arch=modelarch_name,
                          dataset=dataset_name  # model for specified dataset
                   )
 
-# TRANSFER LEARNING
-# try convnet as feature extractor, cause vgg much smaller than image net
-model, criterion, optimizer_conv, exp_lr_scheduler = models.transfer_learning_prep(model, modelarch_name, len(classnames))
+# TRANSFER LEARNING (feature extractor)
+model, criterion, optimizer_conv, exp_lr_scheduler = models.transfer_learning_prep(model,
+                                                                                   modelarch_name, len(classnames))
 
 model_path = './models/'
 if not os.path.exists(model_path): os.mkdir(model_path)
 model_path = model_path + dataset_name + '_on_' + modelarch_name + '.pth'
 
 if not os.path.exists(model_path):
-    # train_model
-    model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler, dataloader, len(dataset_split), epochs=15)
-    # save model
-    torch.save(model.state_dict(), model_path)
+    model = models.train_model(model, criterion, optimizer_conv, exp_lr_scheduler,  # train_model
+                               dataloader, len(dataset_split), epochs=15)
+    torch.save(model.state_dict(), model_path)  # save model
 else:
-    # load trained model
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load(model_path))  # load trained model
     models.visualize_model(model, dataloader, classnames, num_images=6)
 
 # EXPLANATION METHOD
-images, labels = iter(dataloader).next()
+images, labels = iter(dataloader).next() # todo aus imagefolder laden
+# todo einzelnes image einfügen.
 if expl_method == 'grad_cam':
     grad_cam_layer = ''
     saliency = grad_cam(
@@ -94,9 +89,15 @@ elif expl_method == 'gradient':
         input=images,
         target=labels
     )
+elif expl_method == 'meaningful_perturbation':
+    saliency = train_mask(
+        model=model,
+        input=images,
+        targets=labels
+    )
 else:
     assert False, 'Explanation Method is not yet defined'
 
-plot_example(images, saliency, expl_method, labels, classnames)
-print('show plot')
+plot_example(images, saliency, expl_method, labels, classnames, show_plot=True,
+             save_path=expl_method + '_' + classnames[labels[0]]+ '.png')
 
